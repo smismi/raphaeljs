cdc0aee3020b0b18c5adadb255db391ae130a8cb
smismi <smirnov.ru@gmail.com>
2013-06-16 10:31:53 +0400

    raphael zalivka

--- js/01.js
+++ js/01.js
@@ -12,7 +12,13 @@ Raphael.fn.drawGrid = function (x, y, w, h, wv, hv, color) {
     return this.path(path.join(",")).attr({stroke: color});
 };
 
-
+$(function () {
+    $("#data").css({
+        position: "absolute",
+        left: "-9999em",
+        top: "-9999em"
+    });
+});
 
 window.onload = function () {
     function getAnchors(p1x, p1y, p2x, p2y, p3x, p3y) {
@@ -41,22 +47,36 @@ window.onload = function () {
     // Draw
     var width = 800,
         height = 250,
-        leftgutter = 0,
-        bottomgutter = 0,
-        topgutter = 0,
+        leftgutter = 30,
+        bottomgutter = 20,
+        topgutter = 20,
         colorhue = .6 || Math.random(),
         color = "hsl(" + [colorhue, .5, .5] + ")",
         r = Raphael("holder", width, height),
+        txt = {font: '12px Helvetica, Arial', fill: "#fff"},
+        txt1 = {font: '10px Helvetica, Arial', fill: "#fff"},
+        txt2 = {font: '12px Helvetica, Arial', fill: "#000"},
         X = (width - leftgutter) / labels.length,
         max = Math.max.apply(Math, data),
         Y = (height - bottomgutter - topgutter) / max;
-     var path = r.path().attr({stroke: color, "stroke-width": 4, "stroke-linejoin": "round"}),
-        bgp = r.path().attr({stroke: "none", opacity: .3, fill: color});
+    r.drawGrid(leftgutter + X * .5 + .5, topgutter + .5, width - leftgutter - X, height - topgutter - bottomgutter, 10, 10, "#000");
+    var path = r.path().attr({stroke: color, "stroke-width": 4, "stroke-linejoin": "round"}),
+        bgp = r.path().attr({stroke: "none", opacity: .3, fill: color}),
+        label = r.set(),
+        lx = 0, ly = 0,
+        is_label_visible = false,
+        leave_timer,
+        blanket = r.set();
+    label.push(r.text(60, 12, "24 hits").attr(txt));
+    label.push(r.text(60, 27, "22 September 2008").attr(txt1).attr({fill: color}));
+    label.hide();
+//    var frame = r.popup(100, 100, label, "right").attr({fill: "#000", stroke: "#666", "stroke-width": 2, "fill-opacity": .7}).hide();
 
     var p, bgpp;
     for (var i = 0, ii = labels.length; i < ii; i++) {
         var y = Math.round(height - bottomgutter - Y * data[i]),
-            x = Math.round(leftgutter + X * (i + .5));
+            x = Math.round(leftgutter + X * (i + .5)),
+            t = r.text(x, height - 6, labels[i]).attr(txt).toBack();
         if (!i) {
             p = ["M", x, y, "C", x, y];
             bgpp = ["M", leftgutter + X * .5, height - bottomgutter, "L", x, y, "C", x, y];
@@ -70,15 +90,46 @@ window.onload = function () {
             p = p.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
             bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
         }
-
+        var dot = r.circle(x, y, 4).attr({fill: "#333", stroke: color, "stroke-width": 2});
+        blanket.push(r.rect(leftgutter + X * i, 0, X, height - bottomgutter).attr({stroke: "none", fill: "#fff", opacity: 0}));
+        var rect = blanket[blanket.length - 1];
+        (function (x, y, data, lbl, dot) {
+            var timer, i = 0;
+            rect.hover(function () {
+                clearTimeout(leave_timer);
+                var side = "right";
+                if (x + frame.getBBox().width > width) {
+                    side = "left";
+                }
+//                var ppp = r.popup(x, y, label, side, 1),
+                anim = Raphael.animation({
+                    path: ppp.path,
+                    transform: ["t", ppp.dx, ppp.dy]
+                }, 200 * is_label_visible);
+                lx = label[0].transform()[0][1] + ppp.dx;
+                ly = label[0].transform()[0][2] + ppp.dy;
+                frame.show().stop().animate(anim);
+                label[0].attr({text: data + " hit" + (data == 1 ? "" : "s")}).show().stop().animateWith(frame, anim, {transform: ["t", lx, ly]}, 200 * is_label_visible);
+                label[1].attr({text: lbl + " September 2008"}).show().stop().animateWith(frame, anim, {transform: ["t", lx, ly]}, 200 * is_label_visible);
+                dot.attr("r", 6);
+                is_label_visible = true;
+            }, function () {
+                dot.attr("r", 4);
+                leave_timer = setTimeout(function () {
+                    frame.hide();
+                    label[0].hide();
+                    label[1].hide();
+                    is_label_visible = false;
+                }, 1);
+            });
+        })(x, y, data[i], labels[i], dot);
     }
-
-
-
-
     p = p.concat([x, y, x, y]);
     bgpp = bgpp.concat([x, y, x, y, "L", x, height - bottomgutter, "z"]);
     path.attr({path: p});
     bgp.attr({path: bgpp});
 //    frame.toFront();
+    label[0].toFront();
+    label[1].toFront();
+    blanket.toFront();
 };
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function getAnchors(p1x, p1y, p2x, p2y, p3x, p3y) {
     var l1 = (p2x - p1x) / 2,
         l2 = (p3x - p2x) / 2,
@@ -18,12 +20,15 @@ function getAnchors(p1x, p1y, p2x, p2y, p3x, p3y) {
     };
 }
 
+// Grab the data
+var labels = [1, 2, 3, 4, 5, 6, 7],
+    data = [24, 22, 20, 23, 22, 22, 20],
+    data2 = [15, 12, 13, 16, 12, 6, 4];
+
+
+
+
 window.onload = function () {
-    // Grab the data
-
-    var data = [24, 22, 20, 23, 22, 22, 20],
-        data2 = [12, 11, 12, 11, 12, 11, 13];
-
 
 
     // Draw
@@ -31,46 +36,42 @@ window.onload = function () {
         height = 150,
         leftgutter = 0,
         bottomgutter = 0,
-        topgutter = 0,
+        topgutter = 30,
         color = "rgba(255,102,0,1)",
         color2 = "rgba(0,102,153,1)",
         r = Raphael("holder", width, height),
-        min = Math.min.apply(Math, data.concat(data2)),
-        max = Math.max.apply(Math, data.concat(data2));
-
-        X = (width - leftgutter) / data.length,
+        X = (width - leftgutter) / labels.length,
+        max = Math.max.apply(Math, data.concat(data2)),
         Y = (height - bottomgutter - topgutter) / max;
 
-    console.log(min, max);
+    var blanket = r.set();
 
-    var path = r.path().attr({stroke: color, "stroke-width": 2, "stroke-linejoin": "round"}),
-        path2 = r.path().attr({stroke: color2, "stroke-width": 2, "stroke-linejoin": "round"}),
-        bgp = r.path().attr({stroke: "none", opacity: .3, fill: color}),
-        bgp2 = r.path().attr({stroke: "none", opacity: .3, fill: color2}),
-        blanket = r.set();
+    var path = r.path().attr({stroke: color, "stroke-width": 3, "stroke-linejoin": "round"}),
+        bgp = r.path().attr({stroke: "none", opacity: 1, fill : 'url(./img/bg.png)'});
 
+    var path2 = r.path().attr({stroke: color2, "stroke-width": 3, "stroke-linejoin": "round"}),
+        bgp2 = r.path().attr({stroke: "none", opacity: 1, fill: "#fff"});
 
-    day = draw(data);
-    night = draw(data2);
-
+    draw_day = draw(data);
+    draw_night = draw(data2);
 
 
-    path.attr({path: day._p});
-    path2.attr({path : night._p});
+    path.attr({path: draw_day._p});
+    bgp.attr({path: draw_day._bgpp});
 
-    bgp.attr({path: day._p});
-    bgp2.attr({path: night._p});
+    bgp2.attr({path: draw_night._bgpp});
+    path2.attr({path: draw_night._p});
 
+    path.toFront();
+    path2.toFront();
+    blanket.toFront();
 
 
     function draw(data) {
-
-        var p;
-
-        for (var i = 0, ii = data.length; i < ii; i++) {
+        var p, bgpp;
+        for (var i = 0, ii = labels.length; i < ii; i++) {
             var y = Math.round(height - bottomgutter - Y * data[i]),
                 x = Math.round(leftgutter + X * (i + .5));
-
             if (!i) {
                 p = ["M", x, y, "C", x, y];
                 bgpp = ["M", leftgutter + X * .5, height - bottomgutter, "L", x, y, "C", x, y];
@@ -85,55 +86,17 @@ window.onload = function () {
                 bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
             }
 
-
-
-//            var dot = r.circle(x, y, 4).attr({fill: "#333", stroke: color, "stroke-width": 2});
-
             blanket.push(r.rect(leftgutter + X * i, 0, X, height - bottomgutter).attr({stroke: "none", fill: "#fff", opacity: 0}));
             var rect = blanket[blanket.length - 1];
-            (function (x, y, data /*, dot */) {
-                var timer, i = 0;
-                rect.hover(function () {
-//                    clearTimeout(leave_timer);
-                    var side = "right";
-//                    if (x + frame.getBBox().width > width) {
-//                        side = "left";
-//                    }
-//                var ppp = r.popup(x, y, label, side, 1),
-//                    anim = Raphael.animation({
-//                        path: ppp.path,
-//                        transform: ["t", ppp.dx, ppp.dy]
-//                    }, 200 * is_label_visible);
-//                    lx = label[0].transform()[0][1] + ppp.dx;
-//                    ly = label[0].transform()[0][2] + ppp.dy;
-//                    frame.show().stop().animate(anim);
-//                    label[0].attr({text: data + " hit" + (data == 1 ? "" : "s")}).show().stop().animateWith(frame, anim, {transform: ["t", lx, ly]}, 200 * is_label_visible);
-//                    label[1].attr({text: lbl + " September 2008"}).show().stop().animateWith(frame, anim, {transform: ["t", lx, ly]}, 200 * is_label_visible);
-//                    dot.attr("r", 6);
-//                    is_label_visible = true;
-                }, function () {
-//                    dot.attr("r", 4);
-//                    leave_timer = setTimeout(function () {
-//                        frame.hide();
-//                        label[0].hide();
-//                        label[1].hide();
-//                        is_label_visible = false;
-//                    }, 1);
-                });
-            })(x, y, data[i]/*, dot */);
-
         }
 
 
 
-        bgpp = bgpp.concat([x, y, x, y, "L", x, height - bottomgutter, "z"]);
-        bgpp = bgpp.concat([0, 0, 0, 0, "L", 0, height, "z"]);
 
         p = p.concat([x, y, x, y]);
+        bgpp = bgpp.concat([x, y, x, y, "L", x, height - bottomgutter, "z"]);
 
-        return {_p: p, _bgpp: bgpp};
-
+        return {_p: p, _bgpp: bgpp}
     }
-
-
+//    frame.toFront();
 };
